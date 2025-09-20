07184fa0870324988e565ad3317dac49dcfa882b
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-20 00:25:17 +0000

    Exemplo

--- bd.js
+++ bd.js
@@ -2,3 +2,36 @@ const usuario = {
     "email": "admin@email.com",
     "senha": "admin"
 };
+
+const dados = [
+    {
+        "id": ,
+        "titulo": ,
+        "descricao": ,
+        "conteudo": ,
+        "categoria": ,
+        "autor": ,
+        "data": ,
+        "imagem": "assets/imagem/"
+    },
+    {
+        "id": ,
+        "titulo": ,
+        "descricao": ,
+        "conteudo": ,
+        "categoria": ,
+        "autor": ,
+        "data": ,
+        "imagem": "assets/imagem/"
+    },
+    {
+        "id": ,
+        "titulo": ,
+        "descricao": ,
+        "conteudo": ,
+        "categoria": ,
+        "autor": ,
+        "data": ,
+        "imagem": "assets/imagem/"
+    }
+];
--- login.js
+++ login.js
@@ -5,9 +5,14 @@ function verificarCredenciais() {
     const emailInformado = document.getElementById("email").value;
     const senhaInformada = document.getElementById("senha").value;
 
-    if(emailInformado === email) {
-        alert("E-mail informado corretamente!");
-    } else 
+    if (emailInformado === email) {
+        // alert("E-mail informado corretamente!");
+        if (senhaInformada === senha) {
+            // alert("Senha informada corretamente!");
+            window.location = "home.html";
+        } else
+            alert("Senha informada incorretamente!")
+    } else
         alert("E-mail informado incorretamente!");
 
 }
