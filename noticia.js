95b1dd5cb6af2278e11e85f5e0e575de6494c925
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-26 22:31:51 +0000

    Exemplo

---
+++ noticia.js
@@ -0,0 +1,10 @@
+let noticiasCarregadas = false;
+
+function lerNoticias() {
+    if(!noticiasCarregadas) {
+       carregarNoticias();
+       noticiasCarregadas = true;
+    } else {
+        
+    }
+}
