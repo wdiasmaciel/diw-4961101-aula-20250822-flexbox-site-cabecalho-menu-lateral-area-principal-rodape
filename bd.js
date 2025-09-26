54d9d9776d5cf78117cd74fe8fb42db47a47bf4d
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-26 23:40:51 +0000

    Exemplo

--- bd.js
+++ bd.js
@@ -7,7 +7,7 @@ const dados = [
     {
         "id": 0,
         "titulo": "Transporte Público em Belo Horizonte",
-        "descricao": "O transporte público em BH é composto por um extenso sistema de ônibus urbanos, gerido pela BHTRANS, e pelo Metrô BH, que opera na região metropolitana.",
+        "descricao": "O transporte público em BH é composto por um extenso sistema de ônibus urbanos.",
         "conteudo": "O transporte público em BH é composto por um extenso sistema de ônibus urbanos, gerido pela BHTRANS, e pelo Metrô BH, que opera na região metropolitana. Para utilizar os serviços, você pode adquirir o Cartão BHBUS, que permite recargas via dinheiro, Pix ou boleto, diretamente nos pontos ou pelo aplicativo BHBus+. O aplicativo oferece funcionalidades como rastreamento de veículos em tempo real e cálculo de rotas.",
         "categoria": "Transporte",
         "autor": "Ana Clara Pereira",
